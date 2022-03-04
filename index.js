f1142430eefd2e3ab062636533d954038598c8a8
Lara Sampaio <larasampaio2394@gmail.com>
2022-03-04 20:52:59 -0300

    EXPRESS

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const res = require('express/lib/response');
+
+const app = express()
+
+const user = [{id:13, nome: 'Lara', sobrenome:'Sampaio'}, {id:14, nome: 'Jeferson', sobrenome:'Gonzales'}];
+
+app.get("/",(req, res)=>{
+    res.send(user)
+});
+
+app.get('/ user:id', (req, res) =>{
+    const {id} = req.params
+    const aux = user.find(user) => user.id == id
+
+    res.send(aux)
+})
+
+app.listen(3000)

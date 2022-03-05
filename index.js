10e125ab13bb61af3de03f842dd39bfbdd424ca7
Lara Sampaio <larasampaio2394@gmail.com>
2022-03-04 21:07:26 -0300

    commit - m "EXPRESS"

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/",(req, res)=>{
 
 app.get('/ user:id', (req, res) =>{
     const {id} = req.params
-    const aux = user.find(user) => user.id == id
+    const aux = user.find((user) => user.id == id)
 
     res.send(aux)
 })
